11b14c81f99aace12bf4825bfcdf7ef60014a9ae
FelixMcFelix <FelixMcFelix@users.noreply.github.com>
2024-09-28 16:52:01 +0000

    deploy: fe9b156906640a8e6c8135c71d37d758958fc522

---
+++ current/trait.impl/tokio/io/async_seek/trait.AsyncSeek.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["songbird",[["impl AsyncSeek for <a class=\"struct\" href=\"songbird/input/struct.AsyncReadOnlySource.html\" title=\"struct songbird::input::AsyncReadOnlySource\">AsyncReadOnlySource</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[191]}
